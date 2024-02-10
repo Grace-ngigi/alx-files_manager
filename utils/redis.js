b6221970103f83c8ba5ab30bd0f81df8924693f9
Grace-ngigi <gracengigi859@gmail.com>
2024-02-10 10:09:13 +0300

    init file manager

---
+++ utils/redis.js
@@ -0,0 +1,55 @@
+const redis = require('redis');
+
+class RedisClient {
+  constructor() {
+    // constructor tat creates a client to Redis
+    this.client = redis.createClient();
+    // display errors in console
+    this.client.on('error', (err) => {
+      console.error('Redis Error', err);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, value) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(value);
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    const res = await new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', duration, (err, res) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(res);
+      });
+    });
+    return res;
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, res) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(res);
+      });
+    });
+  }
+}
+const redisClient = new RedisClient();
+module.exports = redisClient;
